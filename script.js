71348fff5898cf610deb6cb4358eef1226b479f0
Marcus <marcuslt95@gmail.com>
2021-05-18 14:21:34 +0100

    Created libraryLoad function which loops through library array and displays each book on its own card. Also imlpemented basic responsiveness

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [
+let library = [
     {title: 'The Three Body Problem',
     author: 'Liu Cixin',
     pages: '448',
@@ -11,6 +11,10 @@ let myLibrary = [
     author: 'Liu Cixin',
     pages: '605',
     read: true}];
+
+const container = document.querySelector('#container');
+
+//Functions
 
 const book = function(title, author, pages, read) {
     this.title = title
@@ -25,5 +29,29 @@ book.prototype.info = function() {
 
 const addBookToLibrary = function() {
     const newBook = new book(title, author, pages, read);
-    myLibrary.push(newBook);
+    library.push(newBook);
 }
+
+const bookCardGenerator = function(e)  {
+    const bookCard = document.createElement('div');
+    bookCard.classList.toggle('book-card');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const shelf = document.createElement('p');
+    title.textContent = e.title;
+    author.textContent = e.author;
+    pages.textContent = e.pages;
+    shelf.textContent = e.shelf;
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(shelf);
+    container.appendChild(bookCard);
+}
+
+const libraryLoad = function() {
+    library.forEach(element => bookCardGenerator(element));
+}
+
+libraryLoad();
